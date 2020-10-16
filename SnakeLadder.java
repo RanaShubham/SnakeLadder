4aa3850fdaec65f73564d01316de4e7f80ae086c
Shubham Rana <ranashubham7296@gmail.com>
2020-10-16 11:05:10 +0530

    Welcome to snake and ladder program

---
+++ SnakeLadder.java
@@ -0,0 +1,12 @@
+package SnakeAndLadder;
+
+public class SnakeLadder {
+
+	static final int startPosition = 0;
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome to snake and ladder program");
+	}
+
+}
