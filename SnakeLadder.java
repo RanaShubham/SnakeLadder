d663c0b8368db00c6291c7af5c1bf5ebd45fe4ab
Shubham Rana <ranashubham7296@gmail.com>
2020-10-16 11:11:21 +0530

    Rolling the die

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,12 +1,18 @@
 package SnakeAndLadder;
 
 public class SnakeLadder {
-
-	static final int startPosition = 0;
+	
+	//Constants
+	static final int START_POSITION = 0;
+	
+	//Variables
+	static int die = 0;
 	
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to snake and ladder program");
+		die = (int)Math.floor(Math.random()*7);
+		System.out.println(die);
 	}
 
 }
