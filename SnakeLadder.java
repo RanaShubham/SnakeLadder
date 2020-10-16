1bc30abc75467f8a9d484b2b42043282e88fac1d
Shubham Rana <ranashubham7296@gmail.com>
2020-10-16 11:26:30 +0530

    Play options are added as no play or snake or ladder

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,18 +1,38 @@
 package SnakeAndLadder;
 
-public class SnakeLadder {
-	
-	//Constants
-	static final int START_POSITION = 0;
+public class SnakeLadder {	
 	
 	//Variables
 	static int die = 0;
+	static int currentPosition =0;
+	static int turn = 0;
 	
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to snake and ladder program");
+		
 		die = (int)Math.floor(Math.random()*7);
-		System.out.println(die);
+		turn = (int)Math.floor(Math.random()*3);
+		
+		switch(turn)
+			{
+				case 0: System.out.println("No play");
+						break;
+				case 1: ladder();
+						break;
+				case 2: snake();
+						break;
+			}
+	}
+
+	private static void snake() 
+	{
+		currentPosition = currentPosition - die ;
+	}
+
+	private static void ladder() 
+	{
+		currentPosition = currentPosition + die;
 	}
 
 }
